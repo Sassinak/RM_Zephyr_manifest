75358511dedc5175efb59ee8d0fe1211216ae314
Sassinak <1752500338@qq.com>
2026-01-25 16:27:26 +0800

    新增心跳检测,整理接收数据结构

--- samples/boards/damiao_mc02/motor/src/main.cpp
+++ samples/boards/damiao_mc02/motor/src/main.cpp
@@ -8,32 +8,6 @@
 
 LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 
-// static int register_motor_dev(const struct device *dev)
-// {
-//     if (dev == NULL) {
-//         LOG_ERR("motor dev is NULL");
-//         return -EINVAL;
-//     }
-//     if (!device_is_ready(dev)) {
-//         LOG_ERR("motor dev not ready: %s", dev->name);
-//         return -ENODEV;
-//     }
-
-//     const motor_driver_api_t *api = (const motor_driver_api_t *)dev->api;
-//     if ((api == NULL) || (api->register_motor == NULL)) {
-//         LOG_ERR("motor api not available: %s", dev->name);
-//         return -ENOSYS;
-//     }
-
-//     int ret = api->register_motor(dev);
-//     if (ret < 0) {
-//         LOG_ERR("register_motor failed (%d): %s", ret, dev->name);
-//         return ret;
-//     }
-
-//     LOG_INF("register_motor OK: %s", dev->name);
-//     return 0;
-// }
 
 static const sMotor_data_t *motor_data_from_device(const struct device *dev)
 {
@@ -60,12 +34,14 @@ int main(void)
         return -ENODEV;
     }
 
-    register_dji_motor(motor_fl);
-    register_dji_motor(motor_fr);
+    register_motor(motor_fl);
+    register_motor(motor_fr);
 
     while (true) {
         const sMotor_data_t *fl = motor_data_from_device(motor_fl);
         const sMotor_data_t *fr = motor_data_from_device(motor_fr);
+        // LOG_INF("motor_fl: heartbeat is_alive=%d", get_motor_heartbeat_status(motor_fl));
+        // LOG_INF("motor_fr: heartbeat is_alive=%d", get_motor_heartbeat_status(motor_fr));
 
         LOG_INF("FL angle=%d speed=%d current=%d alive=%d",
             (int)fl->Rx_data.angle, (int)fl->Rx_data.speed, (int)fl->Rx_data.current,
@@ -74,10 +50,8 @@ int main(void)
             (int)fr->Rx_data.angle, (int)fr->Rx_data.speed, (int)fr->Rx_data.current,
             fr->heartbeat_status.is_alive ? 1 : 0);
 
-
         /* Yield so the log processing thread and RTT backend can flush. */
-        k_sleep(K_MSEC(5));
-
+        k_sleep(K_MSEC(500));
     }
 
     return 0;
